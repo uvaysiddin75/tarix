bdbc03a895ae829fa85139a2d07e6c5e4efce237
uvaysiddin75 <uvaysiddin75@gmail.com>
2026-08-20 16:52:56 +0500

    fix: restore full site + live progress (deploy public)

--- public/js/static-config.js
+++ public/js/static-config.js
@@ -5,6 +5,9 @@
     adminEmail: "uvaysiddin75@gmail.com",
     adminPassword: "salmic1023",
     adminName: "Uvaysiddin",
+    extraAdmins: [
+      { email: "abdullaevsalmon7@gmail.com", name: "Salmon" }
+    ],
     registrationEnabled: true,
   };
 })();
